bebacbb39599c65f625d2d6ac72dfb6b98c4c2c1
9.nashi <9.nashi@gmail.com>
2016-01-04 11:47:56 +0000

    Added buzz

--- rigo-counter.c
+++ rigo-counter.c
@@ -6,23 +6,24 @@
 // set pin numbers:
 const int buttonPin = 2;     // the number of the pushbutton pin
 const int baseLedPin = 8;    // 
-const int numPins = 3;
-const int ledPin =  13;      // the number of the LED pin
-
+const int numLeds = 3;
+const int speakerOut = basedLedPin + numLeds;
 const int resetAfter = 300;  // reset the counter after this milli sec duration
 
+
+int buttonState = 0;         // variable for reading the pushbutton status
 int cnt = 0; // the rigo counter
 int lastButtonMs = -1; // last time when the button was pressed
 int pushed = 0; // 
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-
 void setup() {
+  int i;
   Serial.begin(9600);
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  pinMode(baseLedPin, OUTPUT);
+  // initialize the LED pins as output:
+  for (i = 0; i < numLeds; i++) {
+    pinMode(baseLedPin + i, OUTPUT);
+  }
+  pinMode(speakerOut, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
 }
@@ -43,7 +44,7 @@ void lightLed() {
   logTime();
   Serial.println("button pressed");
   Serial.println(cnt);
-  doLed(min(cnt, numPins), HIGH);
+  doLed(min(cnt, numLeds), HIGH);
 }
 
 void unlightLed() {
@@ -51,9 +52,11 @@ void unlightLed() {
   Serial.println("LED cleared");
   cnt = 0;
   lastButtonMs = -1;
-  doLed(numPins, LOW);
+  doLed(numLeds, LOW);
 }
 
+// https://www.arduino.cc/en/Tutorial/PlayMelody
+// https://learn.adafruit.com/multi-tasking-the-arduino-part-1/ditch-the-delay
 void buzz() {
 }
 
@@ -79,24 +82,5 @@ void loop() {
     pushed = 1;
   } else {
     pushed = 0;
-  }
-/* original
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-    digitalWrite(redLedPin, HIGH);
-    cnt++;
-    Serial.print("time:");
-    Serial.println(millis());
-    Serial.print("cnt:");
-    Serial.println(cnt);
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
-    digitalWrite(redLedPin, LOW);
-  }
-  */
-  
+  }  
 }
